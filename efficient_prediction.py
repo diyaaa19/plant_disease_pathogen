dc8b0519acfd216d97a8aede4c8b53dc3a9301a3
Diya Jain <151644762+diyaaa19@users.noreply.github.com>
2026-02-17 10:36:05 +0530

    Update efficient_prediction.py

--- efficient_prediction.py
+++ efficient_prediction.py
@@ -8,13 +8,8 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.efficientnet import preprocess_input
 
-# ------------------ LOAD MODEL ------------------
 model = load_model("colored_pathogen_classifier_efficientnet.h5")
 print("Model loaded successfully!")
-
-# IMPORTANT:
-# Make sure order matches training:
-# print(train_gen.class_indices)
 
 class_labels = [
     "Bacteria",
@@ -24,20 +19,15 @@ class_labels = [
     "Virus"
 ]
 
-# ------------------ PREDICTION FUNCTION ------------------
 def predict_disease(img_path):
 
-    # Load image
     img = image.load_img(img_path, target_size=(224, 224))
     img_array = image.img_to_array(img)
 
-    # Expand dimensions
     img_array = np.expand_dims(img_array, axis=0)
 
-    # IMPORTANT: EfficientNet preprocessing
     img_array = preprocess_input(img_array)
 
-    # Prediction
     prediction = model.predict(img_array)
     predicted_class_index = np.argmax(prediction, axis=1)[0]
     predicted_label = class_labels[predicted_class_index]
@@ -48,7 +38,6 @@ def predict_disease(img_path):
     print(f"Confidence: {confidence:.2f}%")
     print("Raw Probabilities:", prediction)
 
-    # Display image with result
     img_disp = image.load_img(img_path)
     plt.imshow(img_disp)
     plt.axis("off")
@@ -60,7 +49,6 @@ def predict_disease(img_path):
     plt.show()
 
 
-# ------------------ MAIN ------------------
 if __name__ == "__main__":
 
     root = tk.Tk()
@@ -79,3 +67,4 @@ if __name__ == "__main__":
         print(f"File not found: {file_path}")
     else:
         predict_disease(file_path)
+
